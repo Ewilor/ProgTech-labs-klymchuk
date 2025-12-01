6087f112366368b6d7397a57f9d609727ce6aeed
Ewilor <klimka.16.03@gmail.com>
2025-12-01 10:17:08 +0200

    fix

--- tests.py
+++ tests.py
@@ -61,4 +61,4 @@ if __name__ == '__main__':
     
     import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner, exit=False)
+    unittest.main(testRunner=runner)
